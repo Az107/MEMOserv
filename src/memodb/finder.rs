44d435dce14b3165812b76581289bbdda72657e6
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2024-05-21 20:01:09 +0200

    create binary tree

--- src/memodb/finder.rs
+++ src/memodb/finder.rs
@@ -1,65 +1,87 @@
 // Written by Alberto Ruiz 2024-03-08
 // [Unfinished] module to index and create search structures
 //
-// The B-tree will be used to index the documents
+// The Binary tree will be used to index the documents
 // and provide a fast search
 
+use std::collections::btree_map::Values;
 
-// B-tree node
-//      v: value of the content to index
-//      p: position of the document in the collection
-struct MIndex {
-    pub v: i32,
-    pub p: Vec<i32>,
+
+
+struct BinaryNode {
+    value: u64,
+    l: Option<Box<BinaryNode>>, // value > l.value
+    r: Option<Box<BinaryNode>> // value < r.value
 }
 
-//B-tree node
-pub struct BNode {
-    keys: Vec<MIndex>,
-    children: Vec<Box<BNode>>,
+impl BinaryNode {
+    fn new(value: u64) -> Self {
+        BinaryNode {
+            value,
+            l: None,
+            r: None
+        }
+    }
+
+    fn insert(&mut self, new_value: u64) {
+        if new_value < self.value {
+            match self.l {
+                Some(ref mut left) => left.insert(new_value),
+                None => self.l = Some(Box::new(BinaryNode::new(new_value))),
+            }
+        } else {
+            match self.r {
+                Some(ref mut right) => right.insert(new_value),
+                None => self.r = Some(Box::new(BinaryNode::new(new_value))),
+            }
+        }
+    }
 }
 
-impl BNode {
-    pub fn new() -> BNode {
-        BNode {
-            keys: Vec::new(),
-            children: Vec::new(),
-        }
+struct BinaryTree {
+    trunc: Option<BinaryNode>
+}
+
+
+impl BinaryTree {
+    fn new() -> BinaryTree {
+        BinaryTree { 
+            trunc: None
+         }
     }
 
-    fn _search(node: &BNode, value: i32) -> Option<Vec<i32>> {
-        let mut i = 0;
-        while i < node.keys.len() && value > node.keys[i].v {
-            i += 1;
+    fn add(&mut self, v: u64) {
+        match self.trunc {
+            Some(ref mut node) => node.insert(v),
+            None => self.trunc = Some(BinaryNode::new(v))
         }
-        if i < node.keys.len() && value == node.keys[i].v {
-            return Some(node.keys[i].p.clone());
-        }
-        if node.children.len() > 0 {
-            return BNode::_search(&node.children[i], value);
-        }
-        None
+    }
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::BinaryTree;
+
+
+    #[test]
+    fn test_tree() {
+        let mut tree = BinaryTree::new();
+        tree.add(5);
+        assert!(tree.trunc.is_some());
+        assert!(tree.trunc.unwrap().value == 5);
     }
 
-    pub fn search(&self, value: i32) -> Option<Vec<i32>> {
-        BNode::_search(&self, value)
+    #[test]
+    fn test_tree_add() {
+        let mut tree = BinaryTree::new();
+        tree.add(5);
+        tree.add(10);
+        assert!(tree.trunc.is_some());
+        assert!(tree.trunc.as_ref().unwrap().value == 5);
+        assert!(tree.trunc.as_ref().unwrap().r.is_some());
+        assert!(tree.trunc.as_ref().unwrap().r.as_ref().unwrap().value == 10);
+
     }
-
-    pub fn insert(&mut self, value: i32, pos: i32) {
-        let mut i = 0;
-        while i < self.keys.len() && value > self.keys[i].v {
-            i += 1;
-        }
-        if i < self.keys.len() && value == self.keys[i].v {
-            self.keys[i].p.push(pos);
-            return;
-        }
-        if self.children.len() > 0 {
-            self.children[i].insert(value, pos);
-            return;
-        }
-        self.keys.insert(i, MIndex { v: value, p: vec![pos] });
-    }
-
-
 }
